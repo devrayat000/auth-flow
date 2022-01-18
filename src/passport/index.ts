7ad74d648cea6e9a453fb49d71ad16f5fb311024
devRayat <dev.rayat000@gmail.com>
2022-01-18 13:42:46 +0600

    implemented passport serializers

--- src/passport/index.ts
+++ src/passport/index.ts
@@ -1,5 +1,7 @@
+import AllRecipeUser from '$models/all_recipe_user'
 import express from 'express'
 import { Passport } from 'passport'
+import { getConnection } from 'typeorm'
 import initializeLocal from './local'
 
 const passportApp = express()
@@ -7,6 +9,31 @@ const passport = new Passport()
 
 passportApp.use(passport.initialize())
 passportApp.use(passport.session())
+
+passport.serializeUser<string>((user, done) => {
+  try {
+    done(null, user.userId)
+  } catch (error) {
+    done(error)
+  }
+})
+
+passport.deserializeUser<string>(async (userId, done) => {
+  try {
+    const user = await getConnection()
+      .createQueryBuilder(AllRecipeUser, 'user')
+      .where('user.user_id = :userId', { userId })
+      .limit(1)
+      .getOne()
+
+    if (!user) {
+      throw new Error('User not found!')
+    }
+    done(null, user)
+  } catch (error) {
+    done(error)
+  }
+})
 
 initializeLocal(passport)
 
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,3 +1,5 @@
+import { IAllRecipeUser } from '$models/all_recipe_user'
+
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
@@ -6,6 +8,10 @@ declare global {
       readonly SESSION_SECRET?: string
       readonly COOKIE_SECRET?: string
     }
+  }
+
+  namespace Express {
+    interface User extends IAllRecipeUser {}
   }
 
   declare const __DEV__: boolean
